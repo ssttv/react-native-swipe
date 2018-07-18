a3536e769b5e3e00d8bb2b6369bc19b94c514b88
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-18 21:29:47 +0300

    feat: render cards

---
+++ src/Deck.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Animated, Text, StyleSheet } from "react-native";
+
+class Deck extends Component {
+  renderCards() {
+    return this.props.data.map(item => this.props.renderCard(item));
+  }
+
+  render() {
+    return (
+    <View>
+      {this.renderCards()}
+    <View/>
+    );
+  }
+}
+
+export default Deck;
