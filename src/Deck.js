6ee8cbfb13f1fdd4114548eaf2799f68f73af321
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-19 18:13:40 +0300

    feat: plug PanResponder in

--- src/Deck.js
+++ src/Deck.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import { View, Animated, Text, StyleSheet } from "react-native";
+import { View, Animated, PanResponder, Text, StyleSheet } from "react-native";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder };
+  }
+
   renderCards() {
     const { data, renderCard } = this.props;
     return data.map(item => renderCard(item));
   }
 
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      <View {...this.state.panResponder.panHandlers}>{this.renderCards()}</View>
+    );
   }
 }
 
