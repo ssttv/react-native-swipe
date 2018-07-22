de000211c5fcf061c28b367d7b50830816bf201a
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-22 23:11:38 +0300

    chore: rollback debug

--- src/Deck.js
+++ src/Deck.js
@@ -8,8 +8,6 @@ class Deck extends Component {
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        debugger;
-        console.log(gesture);
       },
       onPanResponderRelease: () => {}
     });
